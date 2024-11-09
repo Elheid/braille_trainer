aaa75872e37f6edf09495e0601ada133570ab1eb
Elheid <Elfieleintsvard@mail.ru>
2024-11-09 20:18:32 +0500

    refactorStyle

--- src/styles/whiteArrowButton.ts
+++ src/styles/whiteArrowButton.ts
@@ -1,10 +1,11 @@
 const whiteArrowButton: React.CSSProperties = {
     borderRadius: "8px",
-    padding: "1%",
+    padding: "2%",
     backgroundColor: "white",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    height:"fit-content"
 };
 
 export default whiteArrowButton;
